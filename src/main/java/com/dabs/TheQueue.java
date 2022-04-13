e320e83e85d5c6d908b83d9002ac4145e6f1bb4f
ComputerGuy <vicmee76@hotmail.com>
2022-04-13 22:31:40 +0100

    -- queue changes

--- src/main/java/com/dabs/TheQueue.java
+++ src/main/java/com/dabs/TheQueue.java
@@ -6,16 +6,16 @@ import java.util.Queue;
 public class TheQueue {
 
     public static void main(String[] args) {
-        Queue<Integer> nums = new LinkedList<>();
-        nums.add(10);
-        nums.add(90);
-        nums.add(20);
-        nums.add(19);
-        nums.add(12);
+        Queue<Integer> numbs = new LinkedList<>();
+        numbs.add(10);
+        numbs.add(90);
+        numbs.add(20);
+        numbs.add(19);
+        numbs.add(12);
 
-        System.out.println(nums);
-        nums.poll();
-        System.out.println(nums);
-        System.out.println(nums.size());
+        System.out.println(numbs);
+        numbs.poll();
+        System.out.println(numbs);
+        System.out.println(numbs.size());
     }
 }
