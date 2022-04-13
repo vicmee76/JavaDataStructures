fbce88c40cba54116cadcd902429c8b9442a77a2
ComputerGuy <vicmee76@hotmail.com>
2022-04-13 22:57:00 +0100

    -- implemented sets

---
+++ src/main/java/com/dabs/TheSets.java
@@ -0,0 +1,17 @@
+package com.dabs;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TheSets {
+
+    public static void main(String[] args) {
+        Set<Integer> balls = new HashSet<>();
+        balls.add(1);
+        balls.add(34);
+        balls.add(13);
+
+        System.out.println(balls);
+    }
+
+}
