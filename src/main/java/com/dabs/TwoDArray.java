217881e941af23f8a8c03b3d3669bfe7eff3eb2f
ComputerGuy <vicmee76@hotmail.com>
2022-04-13 19:07:17 +0100

    -- implemented 2 d array

--- src/main/java/com/dabs/TwoDArray.java
+++ src/main/java/com/dabs/TwoDArray.java
@@ -1,7 +1,23 @@
 package com.dabs;
 
+import java.util.Arrays;
+
 public class TwoDArray {
     public static void main(String[] args) {
+        char[][] board = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+        System.out.println(Arrays.deepToString(board));
 
+        char[][] secondBoard = new char[][]{
+                new char[] {'0', '-', '-'},
+                new char[] {'0', '-', '-'},
+                new char[] {'0', '-', '-'},
+        };
+
+        System.out.println(Arrays.deepToString(secondBoard));
     }
 }
